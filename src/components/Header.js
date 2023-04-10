8c63183927af413caaec3cfb0586ba9148e80c16
Valdir Silva <valdipiresba@gmail.com>
2023-04-10 18:55:35 -0300

    Criando componente header da aplicacao

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import { MainMenu } from "./MainMenu"
+
+export const Header = () => {
+    return (
+        <header>
+            <h1>Movies DB</h1>
+
+            <MainMenu />
+        </header>
+    )
+}
